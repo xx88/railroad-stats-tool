eff2bbc12c281656a15a7a274d2c7dd62f00c027
orcax <siriusxyk@gmail.com>
2016-02-15 13:29:07 -0500

    update tool

--- static/js/index.js
+++ static/js/index.js
@@ -1,105 +1,92 @@
 $(function() {
 
-function findRow(r) {
-  ++r;
-  return $("#tbl-data tr:eq(" + r + ")");
-}
-
-function validate(data) {
-  var result = [];
-  for(var i=0;i<data.length;++i) {
-    var countNaN = 0;
-    for(var j=0;j<data[i].length;++j) {
-      if(j == 0) {
-         if(data[i][j].length == 0) data[i][j] = i + 1;
-         data[i][j] = String(data[i][j]);
-      }
-      else if(isNaN(data[i][j])) countNaN++;
-    }
-    if(countNaN == 0) {
-      result.push(data[i]);
-    }
-    else if(countNaN < 4) {
-      findRow(i).addClass("cell-warn");
-      alert("Error Data in this row!");
-      return false;
-    }
-  }
-  if(result.length == 0) {
-    alert("Missing data!");
-    return false;
-  }
-  return result;
-}
-
-$("#btn-submit").click(function() {
-  var data = [];
-  $("#tbl-data tr:gt(0)").each(function(i) {
-    var tmp = [];
-    $(this).removeClass("cell-warn");
-    $(this).find("span").each(function(j) {
-      var val = $(this).text();
-      if(j > 0) val = parseFloat(val);
-      tmp.push(val);
-    });
-    data.push(tmp);
-  });
-  data = validate(data);
-  if(!data) {
-    return false;
-  }
-  $.ajax({
-    type: "POST",
-    url: "estimate", 
-    data: JSON.stringify({"data": data}),
-    contentType: "application/json",
-    dataType: 'json'
-  }).done(function(result) {
-    var data = result['data'];
-    chartData["wald"] = [];
-    chartData["wilson"] = [];
-    chartData["approx2"] = [];
-    chartData["exact"] = [];
-    for(var i=0;i<data.length;++i) {
-      for(var j in data[i]) {
-        chartData[j].push(data[i][j]);
-      }
-    }
-    drawChart("wald");
-  });
-});
-
+var dataTable = "#tbl-data";
+var calTableBtn = "#btn-cal-table";
+var calCsvBtn = "#btn-cal-csv";
+var addRowBtn = "#btn-addrow";
+var exampleTableBtn = "#btn-example-table";
+var exampleCsvBtn = "#btn-example-csv";
+var dataText = "#text-data";
+var intervalSelect = "#sel-interval";
 var chartData = {
   "wald": [],
   "wilson": [],
   "approx2": [],
   "exact": []
 };
-
-$("#btn-addrow").click(function() {
-  var n = parseInt($('#txt-rows').val());
-  if(isNaN(n) || n <= 0) n = 1;
-  while(n-- > 0) {
-    $("#tbl-data tbody").append([
-      "<tr>",
-      "<td><span contenteditable></span></td>",
-      "<td><span contenteditable></span></td>",
-      "<td><span contenteditable></span></td>",
-      "<td><span contenteditable></span></td>",
-      "<td><span contenteditable></span></td>",
-      "</tr>"].join());
-  }
-});
-
-$("#sel-interval").change(function() {
-  drawChart($(this).val());
-});
-
-$("#btn-upload").click(function() {
-});
+var errClass = "danger";
 
 google.charts.load('current', {'packages':['corechart']});
 //google.charts.setOnLoadCallback(drawChart);
+
+String.prototype.format = function() {
+  var formatted = this;
+  for (var i = 0; i < arguments.length; i++) {
+    var regexp = new RegExp('\\{'+i+'\\}', 'gi');
+    formatted = formatted.replace(regexp, arguments[i]);
+  }
+  return formatted;
+};
+
+function rand(low, high) {
+  return Math.floor(Math.random() * high) + low;
+}
+
+function getRow(r) {
+  return $(dataTable + " tbody tr:eq({0})".format(r));
+}
+
+function getCell(r, c) {
+  return $(dataTable + " tbody tr:eq({0}) td:eq({1})".format(r, c));
+}
+
+function validate(data) {
+  var result = {
+    error: [],
+    data: [],
+    msg: ""
+  };
+  for(var i=0;i<data.length;++i) {
+    var nNaN = [], nMiss = [], nCorrect = 0;
+    for(var j=0;j<data[i].length;++j) {
+      if(j == 0) {
+         if(data[i][j].length == 0) data[i][j] = "L" + String(i+1);
+         else data[i][j] = String(data[i][j]);
+         continue;
+      }
+      if(data[i][j].length == 0) {
+        nMiss.push([i, j]);
+        continue;
+      }
+      data[i][j] = parseFloat(data[i][j]);
+      if(isNaN(data[i][j])) {
+        nNaN.push([i, j]);
+        continue;
+      }
+      ++nCorrect;
+    }
+    if(nMiss.length == 4) {
+      continue;
+    }
+    if(nNaN.length > 0) {
+      result.error = nNaN;
+      result.msg = "Input not a number!";
+      return result;
+    }
+    if(nMiss.length > 0) {
+      result.error = nMiss;
+      result.msg = "Data is missing!";
+      return result;
+    }
+    if(nCorrect < 4) {
+      result.error = [[i, nCorrect+1]];
+      result.msg = "Data is missing!";
+      return result;
+    }
+    result.data.push(data[i]);
+  }
+  return result;
+}
 
 function drawChart(dataType) {
   var data = new google.visualization.DataTable();
@@ -126,5 +113,120 @@ function drawChart(dataType) {
   var chart = new google.visualization.LineChart(document.getElementById('div-chart'));
   chart.draw(data, options);
 }
+
+function sample(rows) {
+  var data = [];
+  for(var i=0;i<rows;++i) {
+    var tmp = ["L" + String(i+1)];
+    tmp.push(rand(1, 100));
+    tmp.push(rand(1, 100));
+    tmp.push(rand(1000, 5000));
+    tmp.push(rand(1000, 5000));
+    data.push(tmp);
+  }
+  return data;
+}
+
+function calculate(data) {
+  $.ajax({
+    type: "POST",
+    url: "estimate", 
+    data: JSON.stringify({"data": data}),
+    contentType: "application/json",
+    dataType: 'json'
+  }).done(function(result) {
+    var data = result['data'];
+    chartData["wald"] = [];
+    chartData["wilson"] = [];
+    chartData["approx2"] = [];
+    chartData["exact"] = [];
+    for(var i=0;i<data.length;++i) {
+      for(var j in data[i]) {
+        chartData[j].push(data[i][j]);
+      }
+    }
+    drawChart($(intervalSelect).val());
+  });
+}
+
+/***********************************************************/
+/*                    Event Listeners                      */
+/***********************************************************/
+
+$(calTableBtn).click(function() {
+  var data = [];
+  $(dataTable + " tbody td").removeClass(errClass);
+  $(dataTable + " tbody tr").each(function(i) {
+    var tmp = [];
+    $(this).find("span").each(function(j) {
+      tmp.push($(this).text());
+    });
+    data.push(tmp);
+  });
+  data = validate(data);
+  if(data.error.length > 0) {
+    for(var i=0;i<data.error.length;++i) {
+      var err = data.error[i];
+      getCell(err[0], err[1]).addClass(errClass);
+    }
+    alert(data.msg);
+    return false;
+  }
+  calculate(data.data);
+});
+
+$(addRowBtn).click(function() {
+  var n = parseInt($('#txt-rows').val());
+  if(isNaN(n) || n <= 0) n = 1;
+  while(n-- > 0) {
+    $("#tbl-data tbody").append([
+      "<tr>",
+      "<td><span contenteditable></span></td>",
+      "<td><span contenteditable></span></td>",
+      "<td><span contenteditable></span></td>",
+      "<td><span contenteditable></span></td>",
+      "<td><span contenteditable></span></td>",
+      "</tr>"].join());
+  }
+});
+
+$(intervalSelect).change(function() {
+  drawChart($(this).val());
+});
+
+$(exampleTableBtn).click(function() {
+  $(dataTable + " tbody td").removeClass(errClass);
+  var rows = $(dataTable + " tbody tr").length;
+  var data = sample(rows);
+  for(var i=0;i<rows;++i) {
+    for(var j=0;j<5;++j) {
+      getCell(i, j).find("span").html(data[i][j]);
+    }
+  }
+});
+
+$(calCsvBtn).click(function() {
+  $(dataText).removeClass(errClass);
+  var text = $(dataText).val();
+  var data = text.split('\n');
+  for(var i=0;i<data.length;++i) {
+    data[i] = data[i].split(',');
+  } 
+  data = validate(data);
+  if(data.error.length > 0) {
+    alert(data.msg + String(data.error));
+    return false;
+  }
+  calculate(data.data);
+});
+
+$(exampleCsvBtn).click(function() {
+  var data = sample(6);
+  for(var i=0;i<6;++i) {
+    data[i] = data[i].join(', ');
+  }
+  data = data.join('\n');
+  $(dataText).val(data);
+});
 
 });
